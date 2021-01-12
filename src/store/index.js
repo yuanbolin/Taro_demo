3c3dde65124ba381fbda3f5f1e4bde94ce6c4ac2
苑博林 <1252432332@qq.com>
2021-01-12 16:55:20 +0800

    redux bug修复

--- src/store/index.js
+++ src/store/index.js
@@ -23,6 +23,6 @@ const enhancer = composeEnhancers(
 )
 
 export default function configStore () {
-  const store = createStore(rootReducer, enhancer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  const store = createStore(rootReducer,enhancer)
   return store
 }
